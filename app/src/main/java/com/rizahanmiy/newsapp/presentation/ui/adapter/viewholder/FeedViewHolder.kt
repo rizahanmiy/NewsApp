a69c6a1f8140dc1bb98368d8487619f25b177b12
Rizahan MIY <72764181+rizahanmiy@users.noreply.github.com>
2023-06-16 08:50:35 +0700

    dateFormat

--- app/src/main/java/com/rizahanmiy/newsapp/presentation/ui/adapter/viewholder/FeedViewHolder.kt
+++ app/src/main/java/com/rizahanmiy/newsapp/presentation/ui/adapter/viewholder/FeedViewHolder.kt
@@ -1,11 +1,17 @@
 package com.rizahanmiy.newsapp.presentation.ui.adapter.viewholder
 
 
+import android.os.Build
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.rizahanmiy.newsapp.data.entities.NewsArticlesApi
 import com.rizahanmiy.newsapp.utils.common.loadImage
 import kotlinx.android.synthetic.main.list_item_article.view.*
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class FeedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -18,6 +24,14 @@ class FeedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             tvAuthor.text = newsArticleEntity.author
             tvDesc.text = newsArticleEntity.description
             tvContent.text = newsArticleEntity.content
+            tvTime.text = newsArticleEntity.publishedAt?.let { convertTimeFormat(it) }
         }
+    }
+
+    private fun convertTimeFormat(inputTime: String): String {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("yyyy-MM-dd hh:mm", Locale.getDefault())
+        val date = inputFormat.parse(inputTime)
+        return outputFormat.format(date!!)
     }
 }
